e121e6c7a1cabb445222eaa0d4d31106dfccc993
aj <aj@n33.co>
2014-12-13 14:14:29 -0600

    Removed canUseProperty method (moved to Skel)

--- src/skel-layers.js
+++ src/skel-layers.js
@@ -1679,34 +1679,6 @@ skel.registerPlugin('layers', (function($) {
 
 			/* Main */
 
-				/**
-				 * Temporary element for canUseProperty()
-				 * @type {DOMElement}
-				 */
-				canUseProperty_element: null,
-
-				/**
-				 * Determines if the browser supports a given property.
-				 * @param {string} p Property.
-				 * @return {bool} True if property is supported, false if not.
-				 */
-				canUseProperty: function(p) {
-
-					if (!_.canUseProperty_element)
-						_.canUseProperty_element = document.createElement('div');
-
-					var e = _.canUseProperty_element.style,
-						up = p.charAt(0).toUpperCase() + p.slice(1);
-
-					return	(
-								p in e
-							||	('Moz' + up) in e
-							||	('Webkit' + up) in e
-							||	('O' + up) in e
-							||	('ms' + up) in e
-					);
-
-				},
 
 				/**
 				 * Gets the base font size (in px).
@@ -2188,12 +2160,12 @@ skel.registerPlugin('layers', (function($) {
 
 							// Feature detection for 'transform'.
 								if (_.config.mode == 'transform'
-								&&	(!_.canUseProperty('transform') || !_.canUseProperty('transition')))
+								&&	(!_._.canUseProperty('transform') || !_._.canUseProperty('transition')))
 									_.config.mode = 'position';
 
 							// Feature detection for 'position'.
 								if (_.config.mode == 'position'
-								&&	!_.canUseProperty('transition'))
+								&&	!_._.canUseProperty('transition'))
 									_.config.mode = 'animate';
 
 							// Hack: Force WP to use 'animate' (poor transform/position performance).
